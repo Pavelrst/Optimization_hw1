0e18f14fb4c171ba8e48220cd196f2eb2ea76b38
Uri <sukirstn@campus.technion.ac.il>
2019-04-06 22:13:51 +0300

    part 4 done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import sympy as sy
 import numpy as np
-from sympy import *
-# from sympy.abc import x
-# from sympy.abc import A
 
 
 def main():
-    data = np.array([2, 1, 8])[:, np.newaxis] #column vector
+    '''
+    A testing function for the excercise's required functions
+    :return:
+    '''
+    data = np.array([2, 1, 8])[:, np.newaxis]  # column vector
     mat = np.array([[2, 7, 6],
                     [9, 5, 1],
                     [4, 3, 8]])
@@ -31,12 +31,12 @@ def main():
     print("Gradient =\n", res1[1])
     print("Hessian =\n", res1[2])
 
-    exp_dict= {"phi_val": sin_val,
-               "phi_g": sin_grad,
-               "phi_h": sin_hessian,
-               "h": exp_val,
-               "h'": exp_grad,
-               "h''": exp_hess}
+    exp_dict = {"phi_val": sin_val,
+                "phi_g": sin_grad,
+                "phi_h": sin_hessian,
+                "h": exp_val,
+                "h'": exp_grad,
+                "h''": exp_hess}
 
     res2 = f2(data, exp_dict, nargout=3)
     print("======> f2 output:")
@@ -44,14 +44,35 @@ def main():
     print("Gradient =\n", res2[1])
     print("Hessian =\n", res2[2])
 
-'''
-x - data
-par - value_funct of phi, gradient_funct of phi, hessian_funct of phi, A
+    numdiff_par = dict({'e': mat, 'fun_par': sin_dict, 'gradient': sin_grad})
+    a, b = numdiff(f1, data, numdiff_par, nargout=2)
+    print("\n\n\na=\n", a, "\nb=\n", b)
 
-out - value of f, gradient vvalue of f, hessian value of f 
-'''
+
 def f1(x, par, nargout=3):
-    assert nargout in range(1, 4)
+    '''
+    nonlinear multivariate function, f1:R^n->R, f1(x) = phi(Ax)
+
+    :param x: function argument, vector. x is R^(n x 1)
+    :param par: A dictionary ('struct') of parameters including:
+        for every nargout:
+            'A' : matrix R^(m x n)
+            'val' : pointer to function phi, nonlinear multivariate function R^m -> R
+        for nargout > 1:
+            'grad' : pointer to gradient function of phi
+        for nargout > 2:
+            'hessian' (optional) : pointer to hessian function of phi
+    :param nargout: number of arguments given, mimics nargout of matlab, can be 1,2 or 3
+    :return: depends on nargout.
+        If nargout is 1, returns f1(x)
+        If nargout is 2, returns (f1(x), f1'(x))
+        If nargout is 3, returns (f1(x), f1'(x), f1''(x))
+    '''
+
+    assert isinstance(x, np.ndarray)
+    assert isinstance(nargout, int) and nargout in range(1, 4)
+    assert isinstance(par, dict)
+
     mat = par['A']
     phi_val = par['val'](np.dot(mat, x))
 
@@ -70,14 +91,34 @@ def f1(x, par, nargout=3):
 
     return phi_val, grad, hess
 
-'''
-x - data
-par - value_funct of phi, gradient_funct of phi, hessian_funct of phi
 
-out - value of f, gradient vvalue of f, hessian value of f 
-'''
 def f2(x, par, nargout=3):
-    assert nargout in range(1, 4)
+    '''
+    h2(x) = f(phi(x))
+    f is a nonlinear multivariate function f2: R^n -> R
+
+    :param x: function argument, vector. x is R^(n x 1)
+    :param par: A dictionary ('struct') of parameters including:
+        for every nargout:
+            "phi_val" : pointer to function phi, nonlinear multivariate function R^m -> R
+            "h" : pointer to nonlinear scalar function, h: R -> R
+        for nargout > 1:
+            "grad_phi" : pointer to gradient function of phi
+            "h'" : pointer to derivative function of h
+        for nargout > 2:
+            "hess_phi" : pointer to hessian function of phi
+            "h''" : pointer to second derivative function of h
+    :param nargout: number of arguments given, mimics nargout of matlab, can be 1,2 or 3
+    :return: depends on nargout.
+        If nargout is 1, returns f(x)
+        If nargout is 2, returns (f(x), f'(x))
+        If nargout is 3, returns (f(x), f'(x), f''(x))
+    '''
+
+    assert isinstance(x, np.ndarray)
+    assert isinstance(nargout, int) and nargout in range(1, 4)
+    assert isinstance(par, dict)
+
     phi_val = par["phi_val"](x)
     f2_val = par["h"](phi_val)
     if nargout == 1:
@@ -97,10 +138,21 @@ def f2(x, par, nargout=3):
 
 
 def sin_val(x):
+    '''
+    :param x: A 3x1 vector of floats
+    :return: sin(x1*x2*x3)
+    '''
+    assert isinstance(x, np.ndarray)
     assert len(x) == 3
     return np.sin(x[0] * x[1] * x[2])
 
+
 def sin_grad(x):
+    '''
+    :param x: A 3x1 vector of floats
+    :return: gradient of sin(x1*x2*x3)
+    '''
+    assert isinstance(x, np.ndarray)
     assert len(x) == 3
     prod = x[0] * x[1] * x[2]
     grad = [np.cos(prod) * x[1] * x[2],
@@ -108,7 +160,13 @@ def sin_grad(x):
             np.cos(prod) * x[0] * x[1]]
     return np.array(grad)
 
+
 def sin_hessian(x):
+    '''
+    :param x: A 3x1 vector of floats
+    :return: hessian of sin(x1*x2*x3)
+    '''
+    assert isinstance(x, np.ndarray)
     assert len(x) == 3
     prod = x[0] * x[1] * x[2]
     dx1dx1 = -np.sin(prod) * x[1] * x[1] * x[2] * x[2]
@@ -128,23 +186,83 @@ def sin_hessian(x):
                [dx3dx1, dx3dx2, dx3dx3]]
     return hessian
 
+
 def Task3_1_func(x):
+    assert isinstance(x, np.ndarray)
     return sin_val(x), sin_grad(x), sin_hessian(x)
 
+
 def exp_val(x):
+    assert isinstance(x, np.ndarray)
     return np.exp(x)
+
 
 def exp_grad(x):
+    assert isinstance(x, np.ndarray)
     return np.exp(x)
+
 
 def exp_hess(x):
+    assert isinstance(x, np.ndarray)
     return np.exp(x)
 
+
 def Task3_2_func(x):
-    # This function return value, fist derivative, second derivative
-    # of expression exp(x)
+    assert isinstance(x, np.ndarray)
     assert len(x) == 3
     return exp_val(x), exp_grad(x), exp_hess(x)
+
+
+E_MACHINE = 2 * pow(10, -16)
+
+
+def numdiff(myfunc, x, par, nargout=2):
+    '''
+    computes gradient and hessian of myfunc numerically
+
+    :param myfunc: pointer to either of f1 or f2
+    :param x: Input vector R^(mx1)
+    :param par: a dictionary including keys:
+        'e' : A matrix R^(mxm)
+        'fun_par' : par dictionary for my_fun
+    :param nargout: Like nargout of matlab, can be 1 or 2
+    :return: [gnum, Hnum]
+        gnum : Numerical estimation of function gradient
+        Hnum : Numerical estimation of function Hessian
+    '''
+    assert callable(myfunc)
+    assert isinstance(x, np.ndarray)
+    assert isinstance(par, dict)
+    assert 'e' in par.keys()
+    assert 'fun_par' in par.keys()
+    assert isinstance(nargout, int)
+    assert nargout in range(1, 3)
+
+    max_abs_val_of_x = max(x.min(), x.max(), key=abs)
+    epsilon = pow(E_MACHINE, 1/3) * max_abs_val_of_x
+    e = par['e']
+    my_func_par = par['fun_par']
+
+    grad = []
+    for i in range(0, len(x)):
+        right_g_i = myfunc(x+epsilon*e[i], my_func_par, nargout=2)[1]
+        left_g_i = myfunc((x-epsilon*e[i]), my_func_par, nargout=2)[1]
+        g_i = (right_g_i + left_g_i)/(2*epsilon)
+        grad.append(g_i)
+    grad = np.array(grad)
+
+    if nargout == 1:
+        return grad
+
+    hess = []
+    analytic_grad = par['gradient']
+    assert callable(analytic_grad)
+    for i in range(0, len(x)):
+        h_i = (analytic_grad(x+epsilon*e[i])-analytic_grad(x-epsilon*e[i]))/(2*epsilon)
+        hess.append(h_i)
+    hess = np.array(hess)
+
+    return grad, hess
 
 
 
